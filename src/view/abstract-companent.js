cdf98b6e5baecf423bdc821b6c75a984bf68fe57
Yauheni <kirichenkozhenia@mail.ru>
2021-04-20 20:02:40 +0300

    create abstract class for companents

---
+++ src/view/abstract-companent.js
@@ -0,0 +1,26 @@
+import { createElement } from '../util.js';
+
+export default class AbstractCompanent {
+  constructor() {
+    if (new.target === 'AbstarctCompanent') {
+      throw new Error('The class is not for instantiate. Create extended class by this class and use it');
+    }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error('The method has not implemented. Implement it in an extended class');
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
