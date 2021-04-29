0e5f809649de02c1434614c7f1e1f509225a93a6
Yauheni <kirichenkozhenia@mail.ru>
2021-04-29 22:03:35 +0300

    implement the sort

--- src/presenter/event.js
+++ src/presenter/event.js
@@ -1,7 +1,7 @@
 import EventItemView from '../view/event-list/event-item.js';
 import EventEditorView from '../view/event-editor/event-editor.js';
 import PointView from '../view/point.js';
-import { render, replace } from '../utils/component.js';
+import { render, replace, remove } from '../utils/component.js';
 
 
 export default class Event {
@@ -15,29 +15,32 @@ export default class Event {
   }
 
   init(eventData) {
-
-    if (this._eventData !== eventData) {
-      this._eventData = eventData;
-      this._point = new PointView(this._eventData);
-      this._eventEditor = new EventEditorView(this._eventData);
-
-      this._changeFavoriteStatus = this._changeFavoriteStatus.bind(this);
+    if (this._event === null) {
       this._eventKeydownCallback = this._eventKeydownCallback.bind(this);
-      this._eventEditorSubmitCallback = this._eventEditorSubmitCallback.bind(this);
-      this._pointOpenEditorCallback = this._pointOpenEditorCallback.bind(this);
-      this._eventEditorCloseCallback = this._eventEditorCloseCallback.bind(this);
-
-      this._point.setFavoriteButtonClickHandler(this._changeFavoriteStatus);
-      this._point.setOpenEditorButtonClickHandler(this._pointOpenEditorCallback);
-      this._eventEditor.setClickHandler(this._eventEditorCloseCallback);
-      this._eventEditor.setSubmitHandler(this._eventEditorSubmitCallback);
-
       this._event = new EventItemView();
       this._event.setKeydownHandler(this._eventKeydownCallback);
-
-      render(this._point, this._event);
       render(this._event, this._container);
     }
+
+    if (this._point !== null) {
+      remove(this._point);
+    }
+
+    this._eventData = eventData;
+    this._point = new PointView(this._eventData);
+    this._eventEditor = new EventEditorView(this._eventData);
+
+    this._changeFavoriteStatus = this._changeFavoriteStatus.bind(this);
+    this._eventEditorSubmitCallback = this._eventEditorSubmitCallback.bind(this);
+    this._pointOpenEditorCallback = this._pointOpenEditorCallback.bind(this);
+    this._eventEditorCloseCallback = this._eventEditorCloseCallback.bind(this);
+
+    this._point.setFavoriteButtonClickHandler(this._changeFavoriteStatus);
+    this._point.setOpenEditorButtonClickHandler(this._pointOpenEditorCallback);
+    this._eventEditor.setClickHandler(this._eventEditorCloseCallback);
+    this._eventEditor.setSubmitHandler(this._eventEditorSubmitCallback);
+
+    render(this._point, this._event);
   }
 
   get event() {
@@ -46,6 +49,10 @@ export default class Event {
 
   get eventEditor() {
     return this._eventEditor;
+  }
+
+  remove() {
+    remove(this._event);
   }
 
   _eventKeydownCallback() {
